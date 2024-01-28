0d90ebd66b6e4adbc493c6a450201dcbb56b4c45
Minh Au <auduongminh2004@gmail.com>
2024-01-28 06:08:37 -0500

    real-time speaker identification with multi-threading

--- back-end/model/predict.py
+++ back-end/model/predict.py
@@ -9,8 +9,10 @@ import pyaudio
 import wave
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 from matplotlib.pyplot import specgram
-from scipy import signal
+
+import threading
 
 # save_spec
 # get_person_name
@@ -38,6 +40,25 @@ from scipy import signal
 # x = Flatten(name="Flatten")(x) # Flatten dimensions to for use in FC layers
 # trained_model = Model(inputs=vgg_model.input, outputs=x)
 # transfer_model = Model(inputs=trained_model.input, outputs=trained_model.get_layer('Flatten').output)
+matplotlib.use("agg")
+
+def get_spect(sound, sr, path : str):
+    fig, ax = plt.subplots(1)
+    plt.axis('off')
+    specgram(np.array(sound), Fs=sr)
+    plt.savefig(path, bbox_inches='tight', pad_inches=0)
+    plt.close(fig)
+    return plt.imread(path)
+
+def predict(name, data, transfer_model, clf):
+    # th = threading.current_thread()
+    # data = np.frombuffer(buf, dtype=np.int16)
+    data = np.concatenate(data)
+    spect = get_spect(data, RATE, f"./temp/{name}.jpg")
+    expanded_spect = np.expand_dims(spect, axis=0)
+    features = transfer_model.predict(expanded_spect)
+    print(f"Prediction for {name}: " + str(clf.predict(features)[0]))
+
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -45,6 +66,18 @@ CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
+
+vgg_model = VGG16(weights='imagenet',include_top=False, input_shape=(369, 496, 3))
+
+for i,layer in enumerate(vgg_model.layers):
+    layer.trainable = False
+
+x = vgg_model.output
+x = Flatten(name="Flatten")(x) # Flatten dimensions to for use in FC layers
+trained_model = Model(inputs=vgg_model.input, outputs=x)
+transfer_model = Model(inputs=trained_model.input, outputs=trained_model.get_layer('Flatten').output)
+clf = load('saved_model.joblib') 
+
 
 p = pyaudio.PyAudio()
 
@@ -57,12 +90,26 @@ stream = p.open(format=FORMAT,
 print("* recording")
 
 frames = []
+np_frames = []
+chunks_per_second = int(RATE / CHUNK)
 
-num_chunks = int(RATE / CHUNK * RECORD_SECONDS)
-
+threads = []
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    curr_sec = i // chunks_per_second 
+    
     data = stream.read(CHUNK)
     frames.append(data)
+    
+    np_frames.append(np.frombuffer(data, dtype=np.int16))
+    if i % chunks_per_second == 0 and curr_sec > 1:
+        data = np.copy(np_frames[(curr_sec - 1) * chunks_per_second : curr_sec * chunks_per_second])
+        if curr_sec == 2:
+            print(len(np_frames[(2 - 1) * chunks_per_second : 2 * chunks_per_second]))
+        th = threading.Thread(target=predict, \
+                              args=(curr_sec, data, transfer_model, clf), \
+                                daemon=True)  
+        threads.append(th)
+        th.start()
 
 print("* done recording")
 
@@ -70,43 +117,5 @@ stream.stop_stream()
 stream.close()
 p.terminate()
 
-
-def get_spect(sound, sr, path : str):
-    fig, ax = plt.subplots(1)
-    plt.axis('off')
-    specgram(np.array(sound), Fs=sr)
-    plt.savefig(path, bbox_inches='tight', pad_inches=0)
-    plt.close(fig)
-    return plt.imread(path)
-
-data = [[] for i in range(RECORD_SECONDS)]
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data[i // int(num_chunks / RECORD_SECONDS)].append(np.frombuffer(frames[i], dtype=np.int16))
-
-data = [np.concatenate(d) for d in data]
-spects = []
-for i in range(len(data)):
-    spects.append(get_spect(data[i], RATE, f"out{i}.jpg"))
-
-vgg_model = VGG16(weights='imagenet',include_top=False, input_shape=(369, 496, 3))
-
-for i,layer in enumerate(vgg_model.layers):
-    layer.trainable = False
-
-x = vgg_model.output
-x = Flatten(name="Flatten")(x) # Flatten dimensions to for use in FC layers
-trained_model = Model(inputs=vgg_model.input, outputs=x)
-transfer_model = Model(inputs=trained_model.input, outputs=trained_model.get_layer('Flatten').output)
-
-clf = load('saved_model.joblib') 
-for s in spects[1:]:
-    expanded_spect = np.expand_dims(s, axis=0)
-    features = transfer_model.predict(expanded_spect)
-    print(clf.predict(features))
-
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+for th in threads:
+    th.join()
